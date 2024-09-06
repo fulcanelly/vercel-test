adcab4539c91235bd2714fe74414a30ece92c1f4
fulcanelly <dallasxfulcanelly@gmail.com>
2024-09-06 14:31:28 +0300

    fut: update port

--- app/index.js
+++ app/index.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.get("*", (req, res) => res.json({ok: true}));
 
-app.listen(80, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
